eaa5e09b9d878d5771f8725b7be89dd9dc036a21
itaydafna <itaydafna@gmail.com>
2016-07-02 23:26:02 +0300

    made remote shoot

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var up = document.querySelector('.up'),
     down = document.querySelector('.down'),
-    fire = document.querySelector('.fire');
+    fire = document.querySelector('.fire'),
+    couchPotato = document.querySelector('.couch-potato');
 
 
 var cannon = {
@@ -23,3 +24,31 @@ var cannon = {
 
 up.addEventListener('click',cannon.addDegree);
 down.addEventListener('click',cannon.decreaseDegree);
+fire.addEventListener('click',shoot);
+
+//creating the shoot function for the click event
+
+function Ball(){
+    var rand1 =Math.floor(255*Math.random()),
+        rand2 = Math.floor(255*Math.random()),
+        rand3 = Math.floor(255*Math.random());
+    this.el = document.createElement('div');
+    this.el.className = "ball";
+    this.el.setAttribute('style','transform: rotate('+cannon.deg+'deg); background: rgb('+rand1+','+rand2+','+rand3+');');
+}
+
+Ball.prototype.shoot = function(el){
+    el.appendChild(this.el);
+    var that= this,
+        left = 0,
+        move = function() {
+            left+=6;
+        that.el.style.transform = 'rotate('+cannon.deg+'deg) translateX(-'+left+'px)';
+    }
+    helper.animate(that.el,move,1);
+}
+
+function shoot(){
+    var ball = new Ball();
+    ball.shoot(couchPotato);
+}
