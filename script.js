c95b90717fd1746ca79bea438811070ab5f30251
itaydafna <itaydafna@gmail.com>
2016-07-04 00:35:52 +0300

    fixed when balls shoudl start showing once they have been shot

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ var up = document.querySelector('.up'),
 
 var cannon = {
 
-    deg:-34,
-    el: document.querySelector('.cannon'),
-    addDegree: function(){
-        cannon.deg++;
-        cannon.render();
-    },
-    decreaseDegree: function(){
-        cannon.deg--;
-        cannon.render();
-    },
-    render: function(){
-        cannon.el.setAttribute('style', 'transform: scale(0.5) rotate(' + cannon.deg + 'deg);');
-    }
+        deg: -34,
+        el: document.querySelector('.cannon'),
+        addDegree: function () {
+            cannon.deg++;
+            cannon.render();
+        },
+        decreaseDegree: function () {
+            cannon.deg--;
+            cannon.render();
+        },
+        render: function () {
+            cannon.el.setAttribute('style', 'transform: scale(0.5) rotate(' + cannon.deg + 'deg);');
+        }
 
-},
+    },
     tv = {
         el: document.querySelector('.tv')
     },
@@ -30,36 +30,44 @@ var cannon = {
         song: new Audio('kavod1.mp3')
     };
 
-up.addEventListener('click',cannon.addDegree);
-down.addEventListener('click',cannon.decreaseDegree);
-fire.addEventListener('click',shoot);
+up.addEventListener('click', cannon.addDegree);
+down.addEventListener('click', cannon.decreaseDegree);
+fire.addEventListener('click', shoot);
 
 //creating the shoot function for the click event
 
-function Ball(){
-    var rand1 =Math.floor(255*Math.random()),
-        rand2 = Math.floor(255*Math.random()),
-        rand3 = Math.floor(255*Math.random());
+function Ball() {
+    var rand1 = Math.floor(255 * Math.random()),
+        rand2 = Math.floor(255 * Math.random()),
+        rand3 = Math.floor(255 * Math.random());
     this.el = document.createElement('div');
     this.el.className = "ball";
-    this.el.setAttribute('style','transform: rotate('+cannon.deg+'deg); background: rgb('+rand1+','+rand2+','+rand3+');');
+    this.el.setAttribute('style', 'transform: rotate(' + cannon.deg + 'deg); background: rgb(' + rand1 + ',' + rand2 + ',' + rand3 + ');');
 }
 
-Ball.prototype.shoot = function(el){
+Ball.prototype.shoot = function (el) {
     el.appendChild(this.el);
-    var that= this,
+    var that = this,
         left = 0,
-        angle = function(){ return cannon.deg}(),
-        move = function() {
-            left+=6;
-        that.el.style.transform = 'rotate('+angle+'deg) translateX(-'+left+'px)';
-            if(left>2200){
+        angle = function () {
+            return cannon.deg
+        }(),
+        move = function () {
+            left += 6;
+            that.el.style.transform = 'rotate(' + angle + 'deg) translateX(-' + left + 'px)';
+            //condition which sets when balls will stat showing after they've been shot
+            if (left < 252) {
+                that.el.style.visibility = 'hidden';
+            } else {
+                that.el.style.visibility = 'visible';
+            }
+            if (left > 2200) {
                 el.removeChild(that.el);
                 return true;
             } else if (
-                helper.isCollide(that.el,tv.el)){
+                helper.isCollide(that.el, tv.el)) {
                 el.removeChild(that.el);
-                yoram.el.style.visibility='visible';
+                yoram.el.style.visibility = 'visible';
                 var song = yoram.song;
                 song.volume = 0.015;
                 song.play();
@@ -67,12 +75,12 @@ Ball.prototype.shoot = function(el){
 
             }
 
-    }
-    helper.animate(that.el,move,1);
+        }
+    helper.animate(that.el, move, 1);
 
 }
 
-function shoot(){
+function shoot() {
     var ball = new Ball();
     ball.shoot(couchPotato);
 }
